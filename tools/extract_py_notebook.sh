77ef95c61fc67f7aafdd2c4ec40515b04230e456
jasper <jasper.friedrichs@gmail.com>
2017-07-25 00:05:58 -0700

    removing jupyter notebook code

--- tools/extract_py_notebook.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python ipython3-versioncontrol/py_to_notebook_v4.py -f notebooks/amazon_forest_notebook.py
--- tools/restore_jupyter_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-path="$HOME/.jupyter/jupyter_notebook_config.py"
-backup="$HOME/.jupyter/jupyter_notebook_config_backup.py"
-cp $backup $path
-echo "Backup restored from $backup"
